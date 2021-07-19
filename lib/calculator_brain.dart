8fcbb3732ea60cf3ce0ac505a457f3525c7170c5
aldebarane1 <matriculemat@gmail.com>
2021-07-19 15:01:27 +0200

    ajout catégories

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -10,27 +10,38 @@ class CalculatorBrain {
 
   CalculatorBrain({@required this.height, @required this.weight});
   String getResult() {
-    if (_bmi >= 25) {
-      print('coucou1 $_bmi');
-      return 'SURPOIDS';
+    if (_bmi >= 40) {
+      return 'obésité morbide!';
+    } else if (_bmi >= 35) {
+      return 'obésité sévère';
+    } else if (_bmi >= 30) {
+      return 'obésité modérée';
+    } else if (_bmi >= 25) {
+      return 'surpoids';
+    } else if (_bmi >= 18.5) {
+      return 'poids normal';
+    } else if (_bmi >= 16.5) {
+      return 'maigreur';
     } else {
-      if (_bmi >= 21) {
-        print('coucou2 $_bmi');
-        return 'Normal';
-      } else {
-        print('lol $_bmi');
-        return 'UnderWeight';
-      }
+      return 'dénutrition!!';
     }
   }
 
   String getInterpretation() {
-    if (_bmi >= 25) {
-      return 'GROLARS va faire du sport FEIGNASSE';
+    if (_bmi >= 40) {
+      return 'ATTENTION vous êtes en surpoids CRITIQUE il faut consulter!';
+    } else if (_bmi >= 35) {
+      return 'Vous êtes en grand surpoids important il faut agir!';
+    } else if (_bmi >= 30) {
+      return 'Vous êtes en surpoids, un petit régime ?';
+    } else if (_bmi >= 25) {
+      return 'Votre dépassez la corpulence normal,un petit régime ?';
     } else if (_bmi >= 18.5) {
       return 'Votre corpulence est normal, good job!';
-    } else {
+    } else if (_bmi >= 16.5) {
       return 'Vous êtes trop maigre, il faut manger!';
+    } else {
+      return 'ATTENTION vous êtes bien trop maigre et en DANGER! Il faut consulter.';
     }
   }
 
